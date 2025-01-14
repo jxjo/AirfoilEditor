9903547d71435e4832a01b4959071227e284619c
jxjo <58261241+jxjo@users.noreply.github.com>
2025-01-14 11:38:54 +0100

    preparing v3.0

--- dev/build_release.bat
+++ dev/build_release.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set APP_VERSION=3.0 beta
+set APP_VERSION=3.0_beta
 set WIN_RELEASE=AirfoilEditor_%APP_VERSION%_win
 
 echo.
@@ -13,12 +13,26 @@ echo.
 pause
 
 cd ..
+
+rem ----- clean dist folder ------------
+
+if exist dist\AirfoilEditor\Example.dat       del dist\AirfoilEditor\example.dat  /q
+if exist dist\AirfoilEditor\Example_polars    rd  dist\AirfoilEditor\Example_polars  /s /q
+if exist dist\AirfoilEditor\_internal\AirfoilEditor.settings del dist\AirfoilEditor\_internal\AirfoilEditor.settings  /q
+
+pause 
+
+rem ----- clean releases folder ------------
+
 if     exist releases\%WIN_RELEASE% rd releases\%WIN_RELEASE% /s /q
 if     exist releases\%WIN_RELEASE%.zip del releases\%WIN_RELEASE%.zip  /q
 
-mkdir releases\%WIN_RELEASE%
+mkdir "releases\%WIN_RELEASE%"
 
-xcopy README.pdf                   releases\%WIN_RELEASE%\               /i /q
+rem ----- copy files ------------
+
+if not exist Readme.pdf goto end
+xcopy README.pdf                   releases\%WIN_RELEASE%\   /i /q
 
 xcopy dist\AirfoilEditor           releases\%WIN_RELEASE%\   /s /i /q
 
@@ -30,3 +44,4 @@ powershell Compress-Archive %WIN_RELEASE%\* %WIN_RELEASE%.zip
 cd ..
 dir releases\%WIN_RELEASE% 
 pause
+:end
